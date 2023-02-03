117e5e0e70b28fc262a4c108f84ff6879af3c5e3
Josh Cooper <joshcooper@users.noreply.github.com>
2023-02-02 21:53:39 -0800

    Maybe?

--- configs/projects/puppet-agent.rb
+++ configs/projects/puppet-agent.rb
@@ -137,7 +137,7 @@ project "puppet-agent" do |proj|
   # Set the $DEV_BUILD environment variable to leave headers in place.
   proj.component "cleanup"
 
-  proj.component "pl-ruby-patch"
+#  proj.component "pl-ruby-patch"
 
   unless ENV['DEV_BUILD'].to_s.empty?
     proj.settings[:dev_build] = true
